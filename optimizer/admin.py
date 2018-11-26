d14028e291b0ac73692fe8ef767aea28e8f5c261
bdjos <briandjoseph@gmail.com>
2018-11-25 21:14:26 -0500

    Adding models for system components

--- optimizer/admin.py
+++ optimizer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CreateBattery
+
+admin.site.register(CreateBattery)
--- optimizer/models.py
+++ optimizer/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.db import models, connection
+from django.conf import settings
 
-# Create your models here.
+#def drop_table(table_name):
+#    cursor = connection.cursor()
+#    sql = f"DROP TABLE {table_name};"
+#    cursor.execute(sql)
+
+class CreateBattery(models.Model):
+    bat_name = models.CharField(max_length=10)
+    energy_capacity = models.IntegerField()
+    soc_min = models.IntegerField()
+    soc_max = models.IntegerField()
+    base_cost = models.FloatField()
+    energy_cost = models.FloatField()
+
+    def add_component(self):
+        self.save()
+
+    def __str__(self):
+        return self.bat_name
+
+    class Meta:
+        managed = True
