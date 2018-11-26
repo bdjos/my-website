fdd7f8639d038debd21db980b58238cac2aeccb5
bjoseph <briandjoseph@gmail.com>
2018-11-26 16:34:11 -0500

    Visual changes and updates to forms

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from . import views
+from .views import header_pages
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add_component/', views.add_component, name='add_component'),
-    path('add_component/add_demand/', views.add_demand, name='add_demand')
+    path('', header_pages.index, name='index'),
+    path('add_component/', header_pages.add_component, name='add_component'),
+    path('run_model/', header_pages.run_model, name='run_model'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_demand'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_solar'),
+    path('add_component/add_battery/', header_pages.add_battery, name='add_battery'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_converter'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_generator'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_controller'),
+    path('add_component/add_demand/', header_pages.add_demand, name='add_grid'),
 ]
--- optimizer/views.py
+++ optimizer/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import CreateBattery
+from .forms import CreateBatteryForm
 
+class header_pages():
+    def __init__(self):
+        self.components = CreateBattery.objects.all()
 
-def index(request):
-    return render(request, 'optimizer/main.html')
+    @classmethod
+    def index(cls, request):
+        cls.__init__(cls)
+        return render(request, 'optimizer/main.html', {'components': cls.components})
 
-def add_component(request):
-    return render(request, 'optimizer/add_component.html')
+    @classmethod
+    def run_model(cls, request):
+        cls.__init__(cls)
+        return render(request, 'optimizer/main.html', {'components': cls.components})
 
-def add_demand(request):
-    return render(request, 'optimizer/add_demand.html')
+    @classmethod
+    def add_component(cls, request):
+        cls.__init__(cls)
+        return render(request, 'optimizer/add_component.html', {'components': cls.components})
+
+    @classmethod
+    def add_demand(cls, request):
+        cls.__init__(cls)
+        return render(request, 'optimizer/add_demand.html', {'components': cls.components})
+
+    @classmethod
+    def add_battery(cls, request):
+        cls.__init__(cls)
+        if request.method == "POST":
+            form = CreateBatteryForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+#                post.author = request.user
+#                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = CreateBatteryForm()
+        return render(request, 'optimizer/add_battery.html', {'components': cls.components,'form': form})
