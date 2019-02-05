d860fdb7b1ab38022ba6ddb32356827fd633f59d
Brian Joseph <briandjoseph@gmail.com>
2019-02-04 23:46:03 -0500

    Added iframe for ieso_predict app

--- webmain/urls.py
+++ webmain/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ieso_predict/', views.ieso_predict, name='ieso_predict')
 ]
--- webmain/views.py
+++ webmain/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'webmain/home.html')
+
+def ieso_predict(request):
+    return render(request, 'webmain/ieso_predict.html')
